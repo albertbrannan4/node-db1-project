68479341e8db5911652d7395e88d25c5d1b516a7
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-03-22 18:49:07 -0400

    check id validation

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,20 @@
+const Account = require("./accounts-model");
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
-}
+};
 
 exports.checkAccountNameUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
-exports.checkAccountId = (req, res, next) => {
+exports.checkAccountId = async (req, res, next) => {
   // DO YOUR MAGIC
-}
+  let idIsValid = await Account.getById(req.params.id);
+  if (!idIsValid) {
+    next({ status: 404, message: "account not found" });
+  } else {
+    next();
+  }
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const Account = require("./accounts-model");
+const { checkAccountId } = require("./accounts-middleware");
 router.get("/", async (req, res, next) => {
   // DO YOUR MAGIC
   const AllAccounts = await Account.getAll();
   res.status(200).json(AllAccounts);
 });
 
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
   const specificAccount = await Account.getById(req.params.id);
   res.status(200).json(specificAccount);
@@ -16,17 +17,20 @@ router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
 });
 
 router.use((err, req, res, next) => {
   // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
 });
 
 module.exports = router;
